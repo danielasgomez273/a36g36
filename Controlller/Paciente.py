be0fb591e4d05c20db029112730e73e767112674
David Costa <89800442+DavidCosta92@users.noreply.github.com>
2022-11-08 22:04:49 -0300

    Pruebas crud paciente

--- Controlller/Paciente.py
+++ Controlller/Paciente.py
@@ -1,18 +1,8 @@
-
-# import PacienteConexionSqlite3
-# import Model.PacienteConexionSqlite3
-
-# importing the importlib.util module
 import importlib.util        
-  
-# passing the file name and path as argument
 spec = importlib.util.spec_from_file_location(
   "PacienteConexionSqlite3", "Model\PacienteConexionSqlite3.py")    
-  
-# importing the module as foo 
 ConexionBDpaciente = importlib.util.module_from_spec(spec)        
 spec.loader.exec_module(ConexionBDpaciente)
-
 
 class Paciente():
     def __init__(self, nombre, apellido, telefono, email, contraseña):
@@ -24,7 +14,6 @@ class Paciente():
         self.ID = None 
         ConexionBDpaciente.CreatePaciente(nombre, apellido, telefono, email, contraseña)     
        
-
     def get_ID(self):
         self.ID = ConexionBDpaciente.getIdPacienteByEmail(self.email)
         return self.ID
@@ -46,25 +35,9 @@ class Paciente():
         id = self.get_ID(self)
         ConexionBDpaciente.DeletePacienteByID(id)
 
-
-
 pacientePrueba =  Paciente('david','costa','2644641117572','da91@gmail.com','liuadbva1223')
 pacientePrueba2 =  Paciente('david','costa','2644641117572','da91@gmail.com','liuadbva1223')
 pacientePrueba3 =  Paciente('david','costa','2644641117572','da91@gmail.com','liuadbva1223')
 print("imprimiendo get_ID", pacientePrueba.get_ID())
 print("imprimiendo get_ID", pacientePrueba2.get_ID())
 print("imprimiendo get_ID", pacientePrueba3.get_ID())
-
-    # LLAMADO DE FUNCIONES DE CRUD 
-
-    #nombre_paciente = "'david'"+","
-    #apellido_paciente = "'costa'"+","
-    #telefono_paciente = "'2644641117572'"+","
-    #email_paciente = "'da91@gmail.com'"+","
-    #contraseña_paciente ="'1aa1223'"
-
-
-
-
-# DeletePacienteByID("5")
-# UpdatePacienteByID("6", "nombre_paciente", "David")
