ef3cebf1457dc3567c3bbbe6492abe335646abaf
Daniel Herrera <74433631+DHerrera85@users.noreply.github.com>
2023-05-18 19:15:29 -0300

    Se crea Servicio Ecommerce

--- Front/src/app/ecommerce/servicios-comp/servicios-comp.component.ts
+++ Front/src/app/ecommerce/servicios-comp/servicios-comp.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servicios-comp',
   templateUrl: './servicios-comp.component.html',
   styleUrls: ['./servicios-comp.component.css']
 })
-export class ServiciosCompComponent {
+export class ServiciosCompComponent implements OnInit {
+  servicios_S=[
+    {nombre:"Consulta Diabetologo",
+    descripcion:"Consulta con profesional en Diabetologia",
+    monto:4000},
+  
+    {nombre:"Consulta Cardiologo",
+    descripcion:"Consulta con profesional en Cardiologia",
+    monto:4200},
+  
+    {nombre:"Paquete Completo",
+    descripcion:"Contratacion del servicio completo",
+    monto:12000},
+  
+    {nombre:"Analisis Clinico",
+    descripcion:"Cita para extraccion de sangre",
+    monto:4000}]
 
+    constructor(){}
+    ngOnInit(){}
 }
+
+
--- Front/src/app/pages/soporte/soporte.component.ts
+++ Front/src/app/pages/soporte/soporte.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-
+import { EcommerceServiceService } from 'src/app/servicios/ecommerce-service.service';
 @Component({
   selector: 'app-soporte',
   templateUrl: './soporte.component.html',
   styleUrls: ['./soporte.component.css']
 })
 export class SoporteComponent {
+servicios: { nombre: string; descripcion: string; monto: number; }[];
+constructor (servicio:EcommerceServiceService){
+  this.servicios=servicio.muestraservicio()
+}
+ngOnInit(): void{}
 
 }
