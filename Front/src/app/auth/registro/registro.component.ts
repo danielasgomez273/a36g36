0517f33afe17dc63f5dec27c352b8c8a0e118e37
Maximiliano <maxicovaro@gmail.com>
2023-06-01 19:57:49 -0300

    Se agrega servicio
    Se agrega servicio a formulario reactivo de registro

--- Front/src/app/auth/registro/registro.component.ts
+++ Front/src/app/auth/registro/registro.component.ts
@@ -1,10 +1,102 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder,Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { loginInterface } from 'src/app/servicios/interfaces/loginInterface';
+import { LoginService } from 'src/app/servicios/login.service';
+
+// SE USARAN FORMULARIOS REACTIVOS Y VALIDACIONES SINCRONICAS
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
   styleUrls: ['./registro.component.css']
 })
-export class RegistroComponent {
 
+export class RegistroComponent implements OnInit{
+
+  borde_validacion:string=""
+
+
+  // SE CREA OBJETO TIPO formBuilder 
+
+  profileForm=this.formBuilder.group({
+    email:["",[Validators.required,Validators.email]],
+    pass:["",[Validators.required,Validators.minLength(7)]],
+    name:["",[Validators.required]],
+    last_name:["",[Validators.required]],
+    phone:["",[Validators.required]],
+    birthday:["",[Validators.required]],
+    sex:["",[Validators.required]],
+    
+
+  });
+
+  // SE INYECTA FormBuilder               y   EL SERVICIO   LoginService
+  constructor(private formBuilder:FormBuilder,private router:Router , private serv_login:LoginService){};
+
+// METODO DE BORDES
+  bordeOk(){
+    this.borde_validacion="borde-validacion_ok"
+  }
+
+
+// ////////////////////////////// METODO DE VERIFICACION USER ////////////////////////////
+  verificacionRegistroUser(){
+// si el formulario es valido
+    if(this.profileForm.valid){
+      this.serv_login.login(this.profileForm.value as loginInterface).subscribe({
+          next:(userData) => {
+            console.log(userData);
+          this.router.navigateByUrl("/auth/dash_user")
+          },
+          error: (errorData) => {
+              console.error(errorData);
+          },
+          complete:() => {
+          //alert("Validacion realizada correctamente");
+          }
+
+      });
+      //this.profileForm.reset(); // SI VALIDA CORRECTAMENTE SE REINICIAN LOS VALORES DE LOS CAMPOS
+      //this.direccion="auth/dash_user"
+    } 
+    else{
+
+      // SI NO VALIDA TODOS LOS CAMPOS QUEDAN MARCADO EN ROJO
+      this.profileForm.markAllAsTouched();
+      alert("No se ingresaron correctamente los datos")
+    
+    }
+  }
+
+
+
+  ///// METODOS GET /////
+  get email_GET(){
+    return this.profileForm.controls.email;
+  }
+  get pass_GET(){
+    return this.profileForm.controls.pass;
+  }
+  get name_GET(){
+    return this.profileForm.controls.name;
+  }
+  get last_name_GET(){
+    return this.profileForm.controls.last_name;
+  }
+  get birthday_GET(){
+    return this.profileForm.controls.birthday;
+  }
+  get sex_GET(){
+    return this.profileForm.controls.sex;
+  }
+  get phone_GET(){
+    return this.profileForm.controls.phone;
+  }
+
+
+
+  ngOnInit(): void {}
 }
+
+
