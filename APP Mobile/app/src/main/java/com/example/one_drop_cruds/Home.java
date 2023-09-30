670b30f6b55ea35cdd9d6eefb480f991f3781717
David Costa <89800442+DavidCosta92@users.noreply.github.com>
2023-09-30 12:13:12 -0300

    agregado de funcionalidad para crear archivos y descargarlos

--- APP Mobile/app/src/main/java/com/example/one_drop_cruds/Home.java
+++ APP Mobile/app/src/main/java/com/example/one_drop_cruds/Home.java
@@ -3,30 +3,45 @@ package com.example.one_drop_cruds;
 import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
 import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 
+import androidx.activity.result.contract.ActivityResultContract;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Typeface;
 import android.graphics.pdf.PdfDocument;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.text.TextPaint;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.one_drop_cruds.utils.FilesManager;
 import com.example.one_drop_cruds.utils.UserSessionManager;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -47,12 +62,16 @@ public class Home extends AppCompatActivity {
         textView_welcome.setText("¡¡ Bienvenido, "+userSessionManager.getLoguedUsername()+" !!");
 
         filesManager= new FilesManager(getApplicationContext(), this);
+        this.askForPermissionsStorage();
         // filesManager.onCreatePermissionAsk(); // envia por parametro el activty para poder pedir permisos
+/*
         if(checkPermission()) {
             Toast.makeText(this, "Permiso Aceptado", Toast.LENGTH_LONG).show();
         } else {
             requestPermissions();
         }
+
+ */
     }
 
     // METODOS DE NAVEGACION
@@ -74,135 +93,80 @@ public class Home extends AppCompatActivity {
         Intent pressure = new Intent(this, RegPressureActivity.class);
         startActivity(pressure);
     }
-
-
-    ///////////////////////////// TODA ESTA LOGICA DEBERIA ESTAR EN FILE MANAGER!
-    // permisos
-    private boolean checkPermission() {
-       // int permission1 = ContextCompat.checkSelfPermission(getApplicationContext(), WRITE_EXTERNAL_STORAGE);
-        int writePermission = ContextCompat.checkSelfPermission(this, WRITE_EXTERNAL_STORAGE);
-       // int permission2 = ContextCompat.checkSelfPermission(getApplicationContext(), READ_EXTERNAL_STORAGE);
-        int readPermission = ContextCompat.checkSelfPermission(this, READ_EXTERNAL_STORAGE);
-        return writePermission == PackageManager.PERMISSION_GRANTED && readPermission == PackageManager.PERMISSION_GRANTED;
+    public void btn_export_data(View v){
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.onedrop); // solo para enviar logo de oneDrop a pdf
+        if(filesManager.exportPdfFileReport(bitmap)){
+            Toast.makeText(this, "Se creo el PDF correctamente", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "Error en la creacion del PDF", Toast.LENGTH_LONG).show();
+        }
     }
-    private void requestPermissions() {
-        //este tipo de solicitud es asincrona, por lo que cuando el user acepta o no, se llama al metodo onRequestPermissionsResult
-        System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% se supone que estoy pidiendo permisosss %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% ");
-        ActivityCompat.requestPermissions(this, new String[]{WRITE_EXTERNAL_STORAGE, READ_EXTERNAL_STORAGE}, 200);
-        System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% se supone que estoy pidiendo permisosss %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% ");
 
+
+    //askForPermissionsStorage y onRequestPermissionsResult deben ir en cada pagina que al cargar soliciten permiso a usuarios para acceder archivos
+    public void askForPermissionsStorage(){
+        if(Build.VERSION.SDK_INT > Build.VERSION_CODES.M){
+            // LA VERSION DE API ES MAYOR A 23, DEBEMOS PEDIR LOS PERMISOS EN TIEMPO DE EJECUCION
+            Log.i("TAG","API MAYOR A 23");
+            if(ContextCompat.checkSelfPermission(Home.this, WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+                    && ContextCompat.checkSelfPermission(Home.this, READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+                // Return true si estan autorizados
+                Log.i("TAG","PERMISOS AUTORIZADOS!!!!!");
+              //  this.createFile();
+            } else {
+                Log.i("TAG","Permisos estaban rechazados... se los pido nuevamente");
+                //pregunto si los permisos fueron rechazados anteriormente
+                if(ActivityCompat.shouldShowRequestPermissionRationale(Home.this,WRITE_EXTERNAL_STORAGE )
+                        && ActivityCompat.shouldShowRequestPermissionRationale(Home.this,READ_EXTERNAL_STORAGE )){
+                    Log.i("TAG","El usuario previamente rechazo los permisos");
+                }
+                else {
+                    Log.i("TAG","lOS PERMIS NO ESTAN AUTORIZADOS PERO NO FUERON RECHAZADOS NUNCA..");
+                }
+                // voy a pedir permisos
+                Log.i("TAG","-------------- VOY A PEDIR LOS PERMISOS -------------");
+                ActivityCompat.requestPermissions(Home.this, new String[]{WRITE_EXTERNAL_STORAGE , READ_EXTERNAL_STORAGE}, 200);
+                Log.i("TAG","-------------- VOY A PEDIR LOS PERMISOS -------------");
+            }
+        }
     }
+
     @SuppressLint("MissingSuperCall")
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if(requestCode == 200) {
-            if(grantResults.length > 0) {
-                boolean writeStorage = grantResults[0] == PackageManager.PERMISSION_GRANTED;
-                boolean readStorage = grantResults[1] == PackageManager.PERMISSION_GRANTED;
-                if(writeStorage && readStorage) {
-                    Toast.makeText(this, "Permiso concedido", Toast.LENGTH_LONG).show();
+        if(requestCode == 200){
+            if(grantResults.length >0 && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED){
+                Log.i("TAG","Permis autoo (onRequestPermissionsResult)!!!!!");
+                Toast.makeText(Home.this,"Permis autoo (onRequestPermissionsResult)!", Toast.LENGTH_LONG).show();
+                // this.createFile();
+            } else{
+                Log.i("TAG","El usuario previamente rechazo los permisos (onRequestPermissionsResult)");
+                if(ActivityCompat.shouldShowRequestPermissionRationale(Home.this, WRITE_EXTERNAL_STORAGE)
+                && ActivityCompat.shouldShowRequestPermissionRationale(Home.this, READ_EXTERNAL_STORAGE)){
+                    // TRUE si el usuario nego los permisos, y coloco que no vuelva a ser mostrado el cartelito de solicitud
+                    new AlertDialog.Builder(this).setMessage("Vos tenes que aceptar permis para usar la ap")
+                            .setPositiveButton("Try again", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    // voy a pedir permisos
+                                    Log.i("TAG","solicitando permisos nuevamente en dialog interface");
+                                    ActivityCompat.requestPermissions(Home.this, new String[]{WRITE_EXTERNAL_STORAGE , READ_EXTERNAL_STORAGE}, 200);
+                                }
+                            })
+                            .setNegativeButton("No gracias?", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    // salir?
+                                    Log.i("TAG","Otra vez los permis siguen sin estar aceptados");
+                                }
+                            }).show();
                 } else {
-                    Toast.makeText(this, "Permiso denegado", Toast.LENGTH_LONG).show();
-                    //  finish();
+                    Log.i("TAG","TENES QUE HABILITAR PERMISOS MANUALMENTE");
+                    Toast.makeText(Home.this,"TENES QUE HABILITAR PERMISOS MANUALMENTE", Toast.LENGTH_LONG).show();
                 }
-                System.out.println("--------------------------writeStorage && readStorage-------------------------  ");
-                System.out.println(" -------------------------- writeStorage --------------------------"+writeStorage);
-                System.out.println(" -------------------------- readStorage --------------------------"+readStorage);
-                System.out.println("--------------------------writeStorage && readStorage-------------------------  ");
             }
-        } else {
-            System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% CODIGO DISTINTO A 200 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% ");
         }
+        super.onRequestPermissionsResult(requestCode,permissions,grantResults);
     }
 
-
-    public void btn_export_data(View v){
-        // filesManager.createFile();
-        this.createFile();
-    }
-
-    private String createUniqueName(){
-        String now= new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        return "PDF "+now;
-    }
-
-    // EN BOTON GENERAR PDF, LLAMAR A ESTE METODO
-    public void createFile() {
-
-        String tituloText = "Este es el titulo del documento";
-
-        String descripcionText = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. \n" +
-                "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, \n" +
-                "when an unknown printer took a galley of type and scrambled it to make a type specimen book. \n" +
-                "It has survived not only five centuries, but also the leap into electronic typesetting, \n" +
-                "remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset\n" +
-                " sheets containing Lorem Ipsum passages, and more recently with desktop publishing software\n" +
-                " like Aldus PageMaker including versions of Lorem Ipsum.\n";
-
-
-        PdfDocument pdfDocument = new PdfDocument();
-        Paint paint = new Paint();
-        TextPaint titulo = new TextPaint();
-        TextPaint descripcion = new TextPaint();
-
-
-        PdfDocument.PageInfo paginaInfo = new PdfDocument.PageInfo.Builder(816, 1054, 1).create();
-        PdfDocument.Page pagina1 = pdfDocument.startPage(paginaInfo);
-
-        Canvas canvas = pagina1.getCanvas();
-
-        titulo.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.BOLD));
-        titulo.setTextSize(20);
-        canvas.drawText(tituloText, 10, 150, titulo);
-
-        descripcion.setTypeface(Typeface.defaultFromStyle(Typeface.NORMAL));
-        descripcion.setTextSize(14);
-
-        String[] arrDescripcion = descripcionText.split("\n");
-        int y = 200;
-        for (int i = 0; i < arrDescripcion.length; i++) {
-            canvas.drawText(arrDescripcion[i], 10, y, descripcion);
-            y += 15;
-        }
-        pdfDocument.finishPage(pagina1);
-
-        File file = new File(Environment.getExternalStorageDirectory(), createUniqueName());
-        try {
-/// ERROR DE PERMISOSOOOOO
-/// ERROR DE PERMISOSOOOOO
-/// ERROR DE PERMISOSOOOOO
-/// ERROR DE PERMISOSOOOOO
-/// ERROR DE PERMISOSOOOOO
-/// ERROR DE PERMISOSOOOOO
-/// ERROR DE PERMISOSOOOOO
-/// ERROR DE PERMISOSOOOOO
-/// ERROR DE PERMISOSOOOOO
-/// ERROR DE PERMISOSOOOOO
-/// ERROR DE PERMISOSOOOOO
-/// ERROR DE PERMISOSOOOOO
-
-            System.out.println("++++++++++++++++++++++++++POR CREAR archivo++++++++++++++++++++++++++ ");
-            Toast.makeText(this, "POR CREAR ARCHIVOOOOOOOOOOOOOOO", Toast.LENGTH_LONG).show();
-            pdfDocument.writeTo(new FileOutputStream(file));
-            Toast.makeText(this, "Se creo el PDF correctamente", Toast.LENGTH_LONG).show();
-
-/// ERROR DE PERMISOSOOOOO
-/// ERROR DE PERMISOSOOOOO
-/// ERROR DE PERMISOSOOOOO
-/// ERROR DE PERMISOSOOOOO
-/// ERROR DE PERMISOSOOOOO
-/// ERROR DE PERMISOSOOOOO
-/// ERROR DE PERMISOSOOOOO
-/// ERROR DE PERMISOSOOOOO
-/// ERROR DE PERMISOSOOOOO
-/// ERROR DE PERMISOSOOOOO
-/// ERROR DE PERMISOSOOOOO
-/// ERROR DE PERMISOSOOOOO
-
-        } catch (Exception e) {
-            Toast.makeText(this, "Error => "+e.getCause(), Toast.LENGTH_LONG).show();
-            e.printStackTrace();
-        }
-        pdfDocument.close();
-    }
 }
--- APP Mobile/app/src/main/java/com/example/one_drop_cruds/utils/FilesManager.java
+++ APP Mobile/app/src/main/java/com/example/one_drop_cruds/utils/FilesManager.java
@@ -5,6 +5,8 @@ import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Typeface;
@@ -22,24 +24,29 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class FilesManager {
+    UserSessionManager userSessionManager;
     Context contex;
     Activity activity;
     public FilesManager(Context contex, Activity activity) {
         this.contex = contex;
         this.activity = activity;
-    }
-/*
-    private String createUniqueName(){
-        String now= new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        return "PDF "+now;
+        this.userSessionManager = new UserSessionManager(contex);
     }
 
-    // EN BOTON GENERAR PDF, LLAMAR A ESTE METODO
-    public void createFile() {
+    private String getLoguedUsername(){
+       return userSessionManager.getLoguedUsername();
+    }
+    private String createUniqueFileName(){
+        String now = new SimpleDateFormat("dd-MM-yyyy").format(new Date());
+        return "Reporte ONE DROP - "+getLoguedUsername()+" - "+now+".pdf";
+    }
 
-        String tituloText = "Este es el titulo del documento";
+    private String getAllRegisters(){
+        String result = "Sin resultados \n";
 
-        String descripcionText = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. \n" +
+
+                /*
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. \n" +
                 "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, \n" +
                 "when an unknown printer took a galley of type and scrambled it to make a type specimen book. \n" +
                 "It has survived not only five centuries, but also the leap into electronic typesetting, \n" +
@@ -47,46 +54,63 @@ public class FilesManager {
                 " sheets containing Lorem Ipsum passages, and more recently with desktop publishing software\n" +
                 " like Aldus PageMaker including versions of Lorem Ipsum.\n";
 
+                 */
+        return result;
+    }
+
+    public boolean exportPdfFileReport(Bitmap bitmap) {
+        Boolean exportSuccess = false;
+        String docTitle = "Reportes generados con fecha: "+new SimpleDateFormat("dd/MM/yyyy").format(new Date());
+
+        String descriptionText = getAllRegisters();
+
 
         PdfDocument pdfDocument = new PdfDocument();
         Paint paint = new Paint();
-        TextPaint titulo = new TextPaint();
-        TextPaint descripcion = new TextPaint();
+        TextPaint title = new TextPaint();
+        TextPaint description = new TextPaint();
+
+        PdfDocument.PageInfo pageInfo = new PdfDocument.PageInfo.Builder(816, 1054, 1).create();
+        PdfDocument.Page page = pdfDocument.startPage(pageInfo);
+
+        // DEBERIA VER LA FORMA DE OBTENER ESTOS DATOS DESDE ESTA CLASE Y NO ENVIANDOLOS POR PARAMETROOOOO
+        // DEBERIA VER LA FORMA DE OBTENER ESTOS DATOS DESDE ESTA CLASE Y NO ENVIANDOLOS POR PARAMETROOOOO
+        // DEBERIA VER LA FORMA DE OBTENER ESTOS DATOS DESDE ESTA CLASE Y NO ENVIANDOLOS POR PARAMETROOOOO
+        // DEBERIA VER LA FORMA DE OBTENER ESTOS DATOS DESDE ESTA CLASE Y NO ENVIANDOLOS POR PARAMETROOOOO
+        // DEBERIA VER LA FORMA DE OBTENER ESTOS DATOS DESDE ESTA CLASE Y NO ENVIANDOLOS POR PARAMETROOOOO
+
+        // Bitmap bitmap, bitmapEscala;
+        // bitmap = BitmapFactory.decodeResource(AppCompactActivity.getResources(), R.drawable.logo);
+        Bitmap bitmapScale = Bitmap.createScaledBitmap(bitmap, 80, 80, false);
+        Canvas canvas = page.getCanvas();
+        canvas.drawBitmap(bitmapScale, 368, 20, paint);
 
 
-        PdfDocument.PageInfo paginaInfo = new PdfDocument.PageInfo.Builder(816, 1054, 1).create();
-        PdfDocument.Page pagina1 = pdfDocument.startPage(paginaInfo);
+        title.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.BOLD));
+        title.setTextSize(20);
+        canvas.drawText(docTitle, 10, 150, title);
 
-        Canvas canvas = pagina1.getCanvas();
+        description.setTypeface(Typeface.defaultFromStyle(Typeface.NORMAL));
+        description.setTextSize(14);
 
-        titulo.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.BOLD));
-        titulo.setTextSize(20);
-        canvas.drawText(tituloText, 10, 150, titulo);
-
-        descripcion.setTypeface(Typeface.defaultFromStyle(Typeface.NORMAL));
-        descripcion.setTextSize(14);
-
-        String[] arrDescripcion = descripcionText.split("\n");
+        String[] arrayDescription = descriptionText.split("\n");
         int y = 200;
-        for (int i = 0; i < arrDescripcion.length; i++) {
-            canvas.drawText(arrDescripcion[i], 10, y, descripcion);
+        for (int i = 0; i < arrayDescription.length; i++) {
+            canvas.drawText(arrayDescription[i], 10, y, description);
             y += 15;
         }
-        pdfDocument.finishPage(pagina1);
+        pdfDocument.finishPage(page);
 
-        File file = new File(Environment.getExternalStorageDirectory(), createUniqueName());
+        File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), createUniqueFileName());
         try {
-            System.out.println("++++++++++++++++++++++++++POR CREAR archivo++++++++++++++++++++++++++ ");
-            Toast.makeText(this.contex, "POR CREAR ARCHIVOOOOOOOOOOOOOOO", Toast.LENGTH_LONG).show();
-
             pdfDocument.writeTo(new FileOutputStream(file));
-            Toast.makeText(this.activity, "Se creo el PDF correctamente", Toast.LENGTH_LONG).show();
+            exportSuccess = true;
         } catch (Exception e) {
-            Toast.makeText(this.contex, "Error => "+e.getCause(), Toast.LENGTH_LONG).show();
             e.printStackTrace();
         }
         pdfDocument.close();
+        return exportSuccess;
     }
 
- */
+
 }
