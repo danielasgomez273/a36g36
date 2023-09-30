4415854282dcfa43b9ed31eece367efec5537a27
maxicovaro <maxicovaro@gmail.com>
2023-09-30 15:24:54 -0300

    Se modifica home

--- APP Mobile/app/src/main/java/com/example/one_drop_cruds/Home.java
+++ APP Mobile/app/src/main/java/com/example/one_drop_cruds/Home.java
@@ -3,17 +3,12 @@ package com.example.one_drop_cruds;
 import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
 import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 
-import androidx.activity.result.contract.ActivityResultContract;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
-import android.Manifest;
-import android.annotation.SuppressLint;
 import android.app.AlertDialog;
-import android.content.ContentValues;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -23,11 +18,9 @@ import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Typeface;
 import android.graphics.pdf.PdfDocument;
-import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
-import android.provider.MediaStore;
 import android.text.TextPaint;
 import android.util.Log;
 import android.view.View;
@@ -37,16 +30,15 @@ import android.widget.Toast;
 
 import com.example.one_drop_cruds.utils.FilesManager;
 import com.example.one_drop_cruds.utils.UserSessionManager;
-import com.google.android.material.snackbar.Snackbar;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Home extends AppCompatActivity {
+    private static final int PERMISSION_REQUEST_CODE = 200;
+
     UserSessionManager userSessionManager;
     TextView textView_welcome;
     FilesManager filesManager;
@@ -61,19 +53,6 @@ public class Home extends AppCompatActivity {
         userSessionManager.validateLoguedUser(); // SI NO ESTA LOGUEADO, SE REDIRIGE A LOGIN
         textView_welcome.setText("¡¡ Bienvenido, "+userSessionManager.getLoguedUsername()+" !!");
 
-<<<<<<< HEAD
-        filesManager= new FilesManager(getApplicationContext(), this);
-        this.askForPermissionsStorage();
-        // filesManager.onCreatePermissionAsk(); // envia por parametro el activty para poder pedir permisos
-/*
-        if(checkPermission()) {
-            Toast.makeText(this, "Permiso Aceptado", Toast.LENGTH_LONG).show();
-        } else {
-            requestPermissions();
-        }
-
- */
-=======
         filesManager = new FilesManager(getApplicationContext(), this);
 
         // Agrega el código para el botón "Volver a Inicio" que lleva a ContactoActivity
@@ -85,7 +64,6 @@ public class Home extends AppCompatActivity {
                 startActivity(intent);
             }
         });
->>>>>>> 98d21f4d9abf00a012a4a81568b5f5f88781f971
     }
 
     // METODOS DE NAVEGACION
@@ -108,44 +86,19 @@ public class Home extends AppCompatActivity {
         Intent pressure = new Intent(this, RegPressureActivity.class);
         startActivity(pressure);
     }
-<<<<<<< HEAD
+
     public void btn_export_data(View v){
         Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.onedrop); // solo para enviar logo de oneDrop a pdf
-        if(filesManager.exportPdfFileReport(bitmap)){
-            Toast.makeText(this, "Se creo el PDF correctamente", Toast.LENGTH_LONG).show();
+        if(checkPermission()) {
+            if(filesManager.exportPdfFileReport(bitmap)){
+                Toast.makeText(this, "Se creó el PDF correctamente", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Error en la creación del PDF", Toast.LENGTH_LONG).show();
+            }
         } else {
-            Toast.makeText(this, "Error en la creacion del PDF", Toast.LENGTH_LONG).show();
+            requestPermissions();
         }
     }
-
-
-    //askForPermissionsStorage y onRequestPermissionsResult deben ir en cada pagina que al cargar soliciten permiso a usuarios para acceder archivos
-    public void askForPermissionsStorage(){
-        if(Build.VERSION.SDK_INT > Build.VERSION_CODES.M){
-            // LA VERSION DE API ES MAYOR A 23, DEBEMOS PEDIR LOS PERMISOS EN TIEMPO DE EJECUCION
-            Log.i("TAG","API MAYOR A 23");
-            if(ContextCompat.checkSelfPermission(Home.this, WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
-                    && ContextCompat.checkSelfPermission(Home.this, READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
-                // Return true si estan autorizados
-                Log.i("TAG","PERMISOS AUTORIZADOS!!!!!");
-              //  this.createFile();
-            } else {
-                Log.i("TAG","Permisos estaban rechazados... se los pido nuevamente");
-                //pregunto si los permisos fueron rechazados anteriormente
-                if(ActivityCompat.shouldShowRequestPermissionRationale(Home.this,WRITE_EXTERNAL_STORAGE )
-                        && ActivityCompat.shouldShowRequestPermissionRationale(Home.this,READ_EXTERNAL_STORAGE )){
-                    Log.i("TAG","El usuario previamente rechazo los permisos");
-                }
-                else {
-                    Log.i("TAG","lOS PERMIS NO ESTAN AUTORIZADOS PERO NO FUERON RECHAZADOS NUNCA..");
-                }
-                // voy a pedir permisos
-                Log.i("TAG","-------------- VOY A PEDIR LOS PERMISOS -------------");
-                ActivityCompat.requestPermissions(Home.this, new String[]{WRITE_EXTERNAL_STORAGE , READ_EXTERNAL_STORAGE}, 200);
-                Log.i("TAG","-------------- VOY A PEDIR LOS PERMISOS -------------");
-            }
-        }
-=======
 
     // PERMISOS
     private boolean checkPermission() {
@@ -155,72 +108,53 @@ public class Home extends AppCompatActivity {
     }
 
     private void requestPermissions() {
-        ActivityCompat.requestPermissions(this, new String[]{WRITE_EXTERNAL_STORAGE, READ_EXTERNAL_STORAGE}, 200);
->>>>>>> 98d21f4d9abf00a012a4a81568b5f5f88781f971
+        ActivityCompat.requestPermissions(this, new String[]{WRITE_EXTERNAL_STORAGE, READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
     }
 
-    @SuppressLint("MissingSuperCall")
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if(requestCode == 200){
-            if(grantResults.length >0 && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED){
-                Log.i("TAG","Permis autoo (onRequestPermissionsResult)!!!!!");
-                Toast.makeText(Home.this,"Permis autoo (onRequestPermissionsResult)!", Toast.LENGTH_LONG).show();
-                // this.createFile();
-            } else{
-                Log.i("TAG","El usuario previamente rechazo los permisos (onRequestPermissionsResult)");
+        if(requestCode == PERMISSION_REQUEST_CODE){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED){
+                Log.i("TAG","Permisos otorgados (onRequestPermissionsResult)!!!!!");
+                Toast.makeText(Home.this,"Permisos otorgados (onRequestPermissionsResult)!", Toast.LENGTH_LONG).show();
+                createFile();
+            } else {
+                Log.i("TAG","El usuario previamente rechazó los permisos (onRequestPermissionsResult)");
                 if(ActivityCompat.shouldShowRequestPermissionRationale(Home.this, WRITE_EXTERNAL_STORAGE)
-                && ActivityCompat.shouldShowRequestPermissionRationale(Home.this, READ_EXTERNAL_STORAGE)){
-                    // TRUE si el usuario nego los permisos, y coloco que no vuelva a ser mostrado el cartelito de solicitud
-                    new AlertDialog.Builder(this).setMessage("Vos tenes que aceptar permis para usar la ap")
-                            .setPositiveButton("Try again", new DialogInterface.OnClickListener() {
+                        && ActivityCompat.shouldShowRequestPermissionRationale(Home.this, READ_EXTERNAL_STORAGE)){
+                    // TRUE si el usuario negó los permisos, y colocó que no vuelva a ser mostrado el cartelito de solicitud
+                    new AlertDialog.Builder(this)
+                            .setMessage("Necesitas aceptar permisos para usar la aplicación.")
+                            .setPositiveButton("Reintentar", new DialogInterface.OnClickListener() {
                                 @Override
                                 public void onClick(DialogInterface dialogInterface, int i) {
-                                    // voy a pedir permisos
-                                    Log.i("TAG","solicitando permisos nuevamente en dialog interface");
-                                    ActivityCompat.requestPermissions(Home.this, new String[]{WRITE_EXTERNAL_STORAGE , READ_EXTERNAL_STORAGE}, 200);
+                                    requestPermissions();
                                 }
                             })
-                            .setNegativeButton("No gracias?", new DialogInterface.OnClickListener() {
+                            .setNegativeButton("No gracias", new DialogInterface.OnClickListener() {
                                 @Override
                                 public void onClick(DialogInterface dialogInterface, int i) {
-                                    // salir?
-                                    Log.i("TAG","Otra vez los permis siguen sin estar aceptados");
+                                    // Salir o manejar el caso de permisos no otorgados
+                                    Log.i("TAG","El usuario rechazó los permisos nuevamente");
                                 }
                             }).show();
                 } else {
-<<<<<<< HEAD
-                    Log.i("TAG","TENES QUE HABILITAR PERMISOS MANUALMENTE");
-                    Toast.makeText(Home.this,"TENES QUE HABILITAR PERMISOS MANUALMENTE", Toast.LENGTH_LONG).show();
-=======
-                    Toast.makeText(this, "Permiso denegado", Toast.LENGTH_LONG).show();
->>>>>>> 98d21f4d9abf00a012a4a81568b5f5f88781f971
+                    Log.i("TAG","Debes habilitar los permisos manualmente");
+                    Toast.makeText(Home.this, "Debes habilitar los permisos manualmente", Toast.LENGTH_LONG).show();
                 }
             }
         }
-        super.onRequestPermissionsResult(requestCode,permissions,grantResults);
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
-<<<<<<< HEAD
-}
-=======
     // CREAR PDF
-    public void btn_export_data(View v) {
-        // Verifica los permisos antes de crear el archivo PDF
-        if (checkPermission()) {
-            createFile();
-        } else {
-            requestPermissions();
-        }
-    }
-
     private String createUniqueName() {
         String now = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         return "PDF_"+now+".pdf";
     }
 
-    public void createFile() {
-        String tituloText = "Este es el titulo del documento";
+    private void createFile() {
+        String tituloText = "Este es el título del documento";
         String descripcionText = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. \n" +
                 "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, \n" +
                 "when an unknown printer took a galley of type and scrambled it to make a type specimen book. \n" +
@@ -265,4 +199,3 @@ public class Home extends AppCompatActivity {
         pdfDocument.close();
     }
 }
->>>>>>> 98d21f4d9abf00a012a4a81568b5f5f88781f971
